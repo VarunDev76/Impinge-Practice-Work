914ebb577c4086e8d917a3572181a135c7e515fa
bhanu <bhanu@impingeonline.com>
2017-01-31 17:09:41 +0530

    CRUD Is Complete Through Rails

--- Rails Practice/Assocation/app/controllers/users_controller.rb
+++ Rails Practice/Assocation/app/controllers/users_controller.rb
@@ -1,27 +1,57 @@
 class UsersController < ApplicationController
   
+  def index
+  	@users = User.all
+  end
+
+  def show
+  	@id = params[:id]
+  	@users = User.find(@id)
+  end
+
   def new
-  
+  	@user = User.new
   end
 
   def create
   	@user = User.new(users_params)
-  	if @user.save
+  	if @user.valid?
+  		@user.save
 		@msg = "User Saved Succesfully"
-		redirect_to users_path  		
+		redirect_to :action=>'index'
+	else
+		@errors = @user.errors.full_messages
+  		render 'new'
   	end
   end
 
-  def list
-  
-  end
 
   def destroy
-  
+  	@id = params[:id]
+  	@user = User.find(@id)
+  	@user.destroy
+
+  	redirect_to :action=>"index"
+  end
+
+  def edit
+  	@id = params[:id]
+  	@user = User.find(@id)
   end
 
   def update
-  
+  	@id = params[:id]
+	@user = User.find(@id)  
+
+	if @user.update_attributes(users_params)
+		@msg = "User Will Update "
+		@user = User.all
+		redirect_to :action=>'index'
+	else
+		@errors = @user.errors.full_messages
+		render 'edit'
+	end
+
   end
 
   def users_params
--- Rails Practice/Assocation/app/models/user.rb
+++ Rails Practice/Assocation/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	validates_uniqueness_of :name , :email  
+	validates_presence_of :name , :email ,:password , :address
+	validates :password, length:{ in: 5..20} 
+	validates :name , length:{ in: 3..30}
 end
