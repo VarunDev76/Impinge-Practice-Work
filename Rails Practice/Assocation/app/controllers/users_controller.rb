03d6ec1cd13a9edd73dba7f9c949ecbfc0acfc12
bhanu <bhanu@impingeonline.com>
2017-01-31 13:06:14 +0530

    Generate Model User and Working on view page and controller

--- Rails Practice/Assocation/app/controllers/users_controller.rb
+++ Rails Practice/Assocation/app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
+  
   def new
+  
+  end
+
+  def create
+  	@user = User.new(users_params)
+  	if @user.save
+		@msg = "User Saved Succesfully"
+		redirect_to users_path  		
+  	end
   end
 
   def list
+  
   end
 
   def destroy
+  
   end
 
   def update
+  
   end
+
+  def users_params
+  	return params.require(:user).permit(:name , :email , :password , :address)
+  end
+
 end
--- Rails Practice/Assocation/config/routes.rb
+++ Rails Practice/Assocation/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/list'
-
-  get 'users/destroy'
-
-  get 'users/update'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
