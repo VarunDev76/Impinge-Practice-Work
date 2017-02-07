e01dc5cfa1596b4a50bbebba7f97760ad51ab830
bhanu <bhanu@impingeonline.com>
2017-02-07 19:40:43 +0530

    Multiple Record Save At Once Time

---
+++ Rails Practice/multiple_records_save_at_once_time/db/migrate/20170207140747_create_records.rb
@@ -0,0 +1,11 @@
+class CreateRecords < ActiveRecord::Migration
+  def change
+    create_table :records do |t|
+      t.integer :user_id
+      t.string :key
+      t.string :value
+
+      t.timestamps null: false
+    end
+  end
+end
